a10e177ca0ba45ebe8d1326a2b69d271fa2fcc9e
Nikolaspc <n.perez.cvjetkovic@gmail.com>
2026-01-20 22:00:22 +0100

    ci: implement enterprise pipeline with github actions

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,16 +1,18 @@
+// src/modules/tasks/tasks.module.ts
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { StorageModule } from '../../storage/storage.module';
-import { NotificationsModule } from '../notifications/notifications.module'; // English: Changed from events to notifications
+import { NotificationsModule } from '../notifications/notifications.module';
+import { AttachmentsService } from './attachments.service'; // <--- Importación añadida
 
 @Module({
-  imports: [
-    StorageModule,
-    NotificationsModule, // English: Corrected module name
-  ],
+  imports: [StorageModule, NotificationsModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService,
+    AttachmentsService, // <--- Proveedor añadido
+  ],
   exports: [TasksService],
 })
 export class TasksModule {}
